ff7fb6405f0d6ab11a6b4a14c1c123ebc8c71ad9
frankfang <frankfang1990@gmail.com>
2017-06-01 23:15:47 +0800

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+$(window).on('scroll', function (e) {
+  let $allImages = $('img[data-src]')
+  let viewportHeight = $(window).height()
+  let scrollTop = window.scrollY
+  $allImages.each(function (index, image) {
+    let imageTop = $(image).offset().top
+    if (imageTop -viewportHeight - 100 <= scrollTop) {
+      image.src = image.getAttribute('data-src')
+    }
+  })
+})
+
